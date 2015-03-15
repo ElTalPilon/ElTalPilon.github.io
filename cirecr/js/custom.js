94990f2f7d6ef6fb9b69dd8c74d697fd0cd24648
ElTalPilon <josepablofg@gmail.com>
2015-03-14 18:38:52 -0600

    Facebook meta tags & navbar arreglado

--- cirecr/js/custom.js
+++ cirecr/js/custom.js
@@ -1,3 +1,5 @@
+
+// Cambia el ítem activo en la barra de navegación
 $(".nav a").on("click", function(){
    $(".nav").find(".active").removeClass("active");
    $(this).parent().addClass("active");
