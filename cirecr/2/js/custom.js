70111799238e804412e06fe756aef2bc3be8c9a7
ElTalPilon <josepablofg@gmail.com>
2015-03-25 19:22:07 -0600

    Márgenes arreglados

--- cirecr/2/js/custom.js
+++ cirecr/2/js/custom.js
@@ -1,6 +1,7 @@
+$('.carousel').carousel();
 
 // Cambia el ítem activo en la barra de navegación
-$(".nav a").on("click", function(){
+/*$(".nav a").on("click", function(){
    $(".nav").find(".active").removeClass("active");
    $(this).parent().addClass("active");
-});
+});*/
