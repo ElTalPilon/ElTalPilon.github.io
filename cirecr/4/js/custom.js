7cd276869f52e7c579420cbccba1283c2c1f67f7
Jose Pablo Flores García <josepablofg@gmail.com>
2015-04-28 01:04:00 -0600

    Faltan retoques, y cambiar las imagenes del carrusel

--- cirecr/4/js/custom.js
+++ cirecr/4/js/custom.js
@@ -1,20 +1,25 @@
+$(window).load(function () {
+    $("#carousel-clients-logos").endlessScroll({ width: '100%', height: '200px', steps: -2, speed: 40, mousestop: true });
+});
+
+
 function initialize() {
 	var mapCanvas = document.getElementById('map-canvas');
 
+	var myLatlng = new google.maps.LatLng(9.928379, -84.060886);
+
 	var mapOptions = {
-		center: new google.maps.LatLng(9.9321922,-84.0599107),
+		center: myLatlng,
 		zoom: 15,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	}
-	var map = new google.maps.Map(mapCanvas, mapOptions)
+	var map = new google.maps.Map(mapCanvas, mapOptions);
+
+	var marker = new google.maps.Marker({
+	    position: myLatlng,
+	    map: map,
+	    title:"CIRE S.A."
+	});
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
-/*
-var marker = new google.maps.Marker({
-    position: myLatLng,
-    title:"CIRE S.A."
-});
-
-// To add the marker to the map, call setMap();
-marker.setMap(map);*/
