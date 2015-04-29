9315ab5bb1024bf25688bcec6d73cd89fca39a90
Jose Pablo Flores García <josepablofg@gmail.com>
2015-04-28 22:31:54 -0600

    Still missing stuff (logo mainly)

--- cirecr/4/js/custom.js
+++ cirecr/4/js/custom.js
@@ -1,5 +1,5 @@
 $(window).load(function () {
-    $("#carousel-clients-logos").endlessScroll({ width: '100%', height: '200px', steps: -2, speed: 40, mousestop: true });
+    $("#carousel-clients-logos").endlessScroll({ width: '100%', height: '600px', steps: -2, speed: 40, mousestop: false });
 });
 
 
