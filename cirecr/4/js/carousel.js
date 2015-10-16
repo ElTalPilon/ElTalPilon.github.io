8078274adf3285d365c48cfa3a140421a1bd3a9f
José Pablo Flores <josepablofg@gmail.com>
2015-10-16 09:07:26 -0600

    Clients carousel height fix Contact map mb fix

--- cirecr/4/js/carousel.js
+++ cirecr/4/js/carousel.js
@@ -1,3 +1,9 @@
 $(window).load(function () {
-    $("#carousel-clients-logos").endlessScroll({ width: '100%', height: '600px', steps: -2, speed: 40, mousestop: false });
+	$("#carousel-clients-logos").endlessScroll({ 
+		width: '100%', 
+		height: '130px',
+		steps: -2, 
+		speed: 40, 
+		mousestop: false
+	});
 });
